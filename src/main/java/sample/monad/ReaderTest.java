99568d04c297c871d9230e6d1cb5a751946efc28
Jaromir Radek <jradek@yandex.com>
2020-12-10 16:26:52 +0100

    adds dependency injection demo

--- src/main/java/sample/monad/ReaderTest.java
+++ src/main/java/sample/monad/ReaderTest.java
@@ -3,6 +3,7 @@ package sample.monad;
 public class ReaderTest {
 
   static class Context {
+
     public final String name;
     public final UseCase useCase;
 
@@ -13,6 +14,7 @@ public class ReaderTest {
   }
 
   static class UseCase {
+
     Reader<Context, Integer> getValue() {
       return Reader.<Context>ask()
           .flatMap(ctx -> {
@@ -23,16 +25,17 @@ public class ReaderTest {
   }
 
   static class Presenter {
+
     Reader<Context, Object> getSuperHeroes() {
       return Reader.<Context>ask()
           .flatMap(ctx -> {
-                System.out.println("The name is " + ctx.name);
+            System.out.println("The name is " + ctx.name);
 
-                return ctx.useCase.getValue()
-                    .map(v -> {
-                      System.out.println("Use case value is: " + v);
-                      return Reader.pure(null);
-                    });
+            return ctx.useCase.getValue()
+                .map(v -> {
+                  System.out.println("Use case value is: " + v);
+                  return Reader.pure(null);
+                });
           });
     }
   }
@@ -46,7 +49,7 @@ public class ReaderTest {
   }
 
   static Reader<Context, Object> nav() {
-    System.out.println("<nav>");
+    System.out.println("<nav><!-- not interested in context --></nav>");
     return p();
   }
 
@@ -56,6 +59,46 @@ public class ReaderTest {
           System.out.println("<html arg='" + ctx.name + "'>");
           return nav();
         });
+  }
+
+  static class Logger {
+    private String prefix = "Logger: ";
+    public Logger(String p) {
+      prefix = p;
+    }
+
+    public void accept(String s) {
+      System.out.println(prefix + s);
+    }
+  }
+
+  static class Account {
+
+    private Logger logger = null;
+    private String owner;
+    private double balance;
+
+    public Account open(String owner) {
+      this.owner = owner;
+      logger.accept("opened account for " + owner);
+      return this;
+    }
+
+    public Account credit(double amount) {
+      this.balance += amount;
+      logger.accept("credit " + amount + " for " + owner);
+      return this;
+    }
+
+    public Account debit(double amount) {
+      this.balance -= amount;
+      logger.accept("debit " + amount + " from " + owner);
+      return this;
+    }
+
+    public void setLogger(Logger l) {
+      logger = l;
+    }
   }
 
 
@@ -69,6 +112,36 @@ public class ReaderTest {
         .apply(new Context("Michael", new UseCase()));
 
     html().apply(new Context("heino", null));
+
+//    Account account = new Account();
+//    account.open("Alice")
+//        .credit(200.0)
+//        .debit(100);
+
+    // lift the 'logger' "dependency" into the account
+    Account firstAccount = new Account();
+    Reader<Logger, Account> reader = Reader.lift(firstAccount, (a, l) -> a.setLogger(l));
+    reader.map(a -> a.open("Alice"))
+        .map(a -> a.credit(200.0))
+        .map(a -> a.debit(100))
+        .flatMap(a -> {
+            a.debit(10);
+            return Reader.ask();
+        })
+        .map(l -> {
+          l.accept("--- switching accounts ---");
+          return l;
+        })
+        .flatMap(a -> Reader.lift(new Account(), (acc, l) -> acc.setLogger(l)))
+        .map(a -> a.open("Bob"))
+        .map(a -> a.credit(666))
+        .flatMap(a -> Reader.ask())
+        .map(l -> {
+          l.accept("--- switching accounts back ---");
+          return firstAccount;
+        })
+        .map(a -> a.debit(333))
+        .apply(new Logger("The Logger: "));
   }
 
 }
---
+++ src/main/java/sample/monad/package-info.java
@@ -0,0 +1,10 @@
+/**
+ Inspired by https://github.com/enelson/java_monads
+
+ For Reader monad:
+ https://www.slideshare.net/mariofusco/monadic-java
+ slide 66 ff
+
+ https://medium.com/@JorgeCastilloPr/kotlin-dependency-injection-with-the-reader-monad-7d52f94a482e
+ */
+package sample.monad;
